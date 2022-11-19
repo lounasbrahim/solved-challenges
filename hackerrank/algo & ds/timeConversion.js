22eb151ce106e94bd2f897bf3c8de47b183bb91e
loun <lounas1994@gmail.com>
2022-11-19 14:03:19 +0100

    add another timeConversion  solution

---
+++ hackerrank/algo & ds/timeConversion.js
@@ -0,0 +1,18 @@
+// https://www.hackerrank.com/challenges/time-conversion/problem
+
+function timeConversion(s) {
+  const arr = s.slice(0, 8).split(':')
+  arr[0] =
+    s.indexOf('PM') > -1
+      ? arr[0] == 12
+        ? '12'
+        : Number(arr[0]) + 12
+      : arr[0] == 12
+      ? '00'
+      : arr[0]
+  return arr.join(':')
+}
+
+;[0, 1, 2, 3, 4, 5, 6]
+
+3
